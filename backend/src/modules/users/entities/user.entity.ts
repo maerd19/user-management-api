3a3c50bd4df79190768fe3331901e016752b2199
Ivan Alejandro Hernandez <revolutions_333@hotmail.com>
2025-11-25 12:47:02 -0600

    feat(backend): create User entity with all required fields

---
+++ backend/src/modules/users/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true, length: 255 })
+  @Index('idx_users_email')
+  email: string;
+
+  @Column({ length: 255, select: false })
+  password: string;
+
+  @Column({ name: 'first_name', length: 100 })
+  firstName: string;
+
+  @Column({ name: 'last_name', length: 100 })
+  lastName: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
